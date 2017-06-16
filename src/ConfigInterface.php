6df6d617a843ac265815489ff0b2ee98cd8027e7
Igor Vorobiov <igor.vorobioff@gmail.com>
2017-06-16 18:28:19 +0300

    added config interface

---
+++ src/ConfigInterface.php
@@ -0,0 +1,21 @@
+<?php
+namespace ImmediateSolutions\Support\Framework;
+
+/**
+ * @author Igor Vorobiov<igor.vorobioff@gmail.com>
+ */
+interface ConfigInterface
+{
+    /**
+     * @param string $path
+     * @param mixed $default
+     * @return mixed
+     */
+    public function get($path, $default);
+
+    /**
+     * @param string $path
+     * @return bool
+     */
+    public function has($path);
+}
